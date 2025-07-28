3058579e2a17e1dca19e5ed5a641a8ad5a56a394
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-28 11:51:12 -0300

    Update 04_divisor_e_resto.py

---
+++ 04_divisor_e_resto.py
@@ -0,0 +1,14 @@
+'''
+Escreva um programa que leia dois números e imprima o resultado da divisão inteira do 
+primeiro pelo segundo, assim como o resto da divisão, usando o comando while e o 
+operador aritmético de subtração. Lembre-se que podemos entender o quociente da 
+divisão de dois números como a quantidade de vezes que podemos retirar o divisor do 
+dividendo. Logo, 20 ÷ 4 = 5, pois podemos subtrair 4 cinco vezes de 20.  
+'''
+
+primeiro_num = int( input("Digite o primeiro número: "))
+segundo_num = int( input ("Digite o segundo número: "))
+resto = 0
+
+while ((primeiro_num - segundo_num) > segundo_num):
+    resultado =
