d7af02da27d1adae9f4761b653244bb2adfbc9a3
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-31 16:43:39 -0300

    Update 03_taxa_da_populacao.py

--- 03_taxa_da_populacao.py
+++ 03_taxa_da_populacao.py
@@ -8,13 +8,11 @@ Y, mantidas as taxas de crescimento.
 populacao_x = 70000
 populacao_y = 180000
 anos = 0
-porcentagem_x = 3.5 / 100
-porcentagem_y = 1.5 / 100
+taxa_x = 0.035
+taxa_y = 0.015
 
 while (populacao_x <= populacao_y):
     anos += 1
-    populacao_x = populacao_x + porcentagem_x
-    populacao_y = populacao_y + porcentagem_y
+    populacao_x += populacao_x * taxa_x
+    populacao_y += populacao_y * taxa_y
 print(f'Levaram {anos} anos para X superar ou igualar a Y.')
-
-REVISAR ESSE AQUI
